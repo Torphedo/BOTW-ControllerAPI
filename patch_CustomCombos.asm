19da9f6dd9d6f0f94f8903b5f0e80a45e7a70b92
Torphedo <ahiatt.cosgrove@gmail.com>
2022-07-06 09:05:57 -0400

    More comments, fix stick vectors

--- patch_CustomCombos.asm
+++ patch_CustomCombos.asm
@@ -22,12 +22,24 @@ lhz r4, Button_Y (r3) ; Check Y
 cmpw cr0, r4, r6
 beq ExitCodecave
 
-; Proceed to play event if all checks pass
+; Proceed to play event, only if none of the branches to ExitCodecave are triggered
 bla PlayEvent
 b ExitCodecave
 
-; In the example code above, I checked R, ZR, and Y. Here's a list of all the inputs you can check:
-; Note: all buttons are half-words (2 bytes) unless stated otherwise
+; In the example code above, I checked R, ZR, and Y. All listed buttons are half-words (2 bytes)
+; unless stated otherwise. This means you need to load them into a register with a lhz instruction.
+; Double-word inputs need to be loaded into 2 separate registers instead of 1, like so:
+;
+; lwz r4, Stick_L_Vector2f@ha (r3)
+; lwz r5, Stick_L_Vector2f@l (r3)
+; cmpw cr0, r4, r6
+; beq ExitCodecave
+; cmpw cr0, r5, r6
+; beq ExitCodecave
+;
+; In this case 1 of the registers holds the X axis data for LS, and the other holds Y axis data.
+;
+; Here's a list of all the inputs you can check:
 ;
 ; Button_A
 ; Button_B
@@ -39,15 +51,23 @@ b ExitCodecave
 ; Button_ZR
 ; Stick_L_Click
 ; Stick_L_Vector2f (double word)
+; Stick_L_Up
+; Stick_L_Down
+; Stick_L_Left
+; Stick_L_Right
 ; Stick_R_Click
 ; Stick_R_Vector2f (double word)
+; Stick_R_Up
+; Stick_R_Down
+; Stick_R_Left
+; Stick_R_Right
 ; Button_Select
 ; Button_Select_2
 ; Button_Start
 ; Button_Start_2
 ; Button_Touchscreen
 ; Button_DPad_Down
-; TimeSinceLastInput (double word)
+; TimeSinceLastInput (single word, read with this instruction: lwz r4, TimeSinceLastInput@l (r3))
 
 ; Advanced Info:
 ;
@@ -60,8 +80,8 @@ b ExitCodecave
 ;
 ; The half-words at 0x38, 0x48, 0x4C, and 0x4E are in the same section as the others listed above, but
 ; don't seem to correspond to any button. There's another set of offsets starting at 0x68, which appear
-; to be in the same order (in memory) as the ones above, also half words. These ones appear to increment
-; by 0x222 each frame, which is a little weird.
+; to be in the same order (in memory) as the ones above. These ones appear to increment by 0x222 each
+; frame, which is strange.
 ;
 ; The bytes at 0x10D and 0x10C will have different values depending on the rotation of the left and
 ; right stick respectively, but only if they're past a certain threshold from the center.
@@ -99,9 +119,17 @@ Button_R = 0x44
 Button_ZL = 0x2C
 Button_ZR = 0x32
 Stick_L_Click = 0x36
-Stick_L_Vector2f = 0x50
+Stick_L_Vector2f = 0x118
+Stick_L_Up = 0x50
+Stick_L_Down = 0x52
+Stick_L_Left = 0x54
+Stick_L_Right = 0x56
 Stick_R_Click = 0x34
-Stick_R_Vector2f = 0x58
+Stick_R_Vector2f = 0x123
+Stick_R_Up = 0x58
+Stick_R_Down = 0x5A
+Stick_R_Left = 0x5C
+Stick_R_Right = 0x5E
 Button_Select = 0x3A
 Button_Select_2 = 0x40
 Button_Start = 0x3C
