5143c3b5868652fb0d7fb8d64fb5ba397139b29c
Torphedo <ahiatt.cosgrove@gmail.com>
2022-07-06 06:06:49 -0400

    Switch to variables
    No more cool ASCII table :'(

--- patch_CustomCombos.asm
+++ patch_CustomCombos.asm
@@ -12,13 +12,13 @@ EntryPointName:
 CustomCombos:
 ; Read controller data
 li r6, 0
-lhz r4, 0x44 (r3) ; Check R
+lhz r4, Button_R (r3) ; Check R
 cmpw cr0, r4, r6
 beq ExitCodecave
-lhz r4, 0x32 (r3) ; Check ZR
+lhz r4, Button_ZR (r3) ; Check ZR
 cmpw cr0, r4, r6
 beq ExitCodecave
-lhz r4, 0x30 (r3) ; Check Y
+lhz r4, Button_Y (r3) ; Check Y
 cmpw cr0, r4, r6
 beq ExitCodecave
 
@@ -26,65 +26,45 @@ beq ExitCodecave
 bla PlayEvent
 b ExitCodecave
 
-; This table has hex offsets for every button I could find. They should be used as an
-; offset from r3 to load a halfword into another register, like this example to check A:
+; In the example code above, I checked R, ZR, and Y. Here's a list of all the inputs you can check:
+; Note: all buttons are half-words (2 bytes) unless stated otherwise
 ;
-;                      lhz r4, 0x28 (r3)
-;
-; Then do a compare between the register you wrote to and a register that's been set to 0:
-;
-;                      li r6, 0
-;                      cmpw cr0, r4, r6
-;
-; An equal result would mean the targeted button isn't being pressed, so you should follow
-; this up with a Branch If Equal instruction:
-;
-;                      beq ExitCodecave
-;
-; TLDR: Copy the instructions listed above, and paste a hex code from the table below over 
-; 0x28 to check for your preferred button. To check a multi-button combo, repeat these
-; instructions with a different hex code.
-
-; //////////////////////////////////////////////
-; ||     Button Name     ||     Hex Code      ||
-; ||==========================================||
-; ||          A          ||       0x28        ||
-; ||          B          ||       0x2A        ||
-; ||          X          ||       0x2E        ||
-; ||          Y          ||       0x30        ||
-; ||          L          ||       0x42        ||
-; ||          R          ||       0x44        ||
-; ||          ZL         ||       0x2C        ||
-; ||          ZR         ||       0x32        ||
-; ||   Left Stick Click  ||       0x36        ||
-; ||  Right Stick Click  ||       0x34        ||
-; ||      Select / -     ||   0x3A / 0x40     ||
-; ||      Start / +      ||   0x3C / 0x3E     ||
-; ||      Touchscreen    ||       0x46        ||
-; ||      D-Pad Down     ||       0x4A        ||
-; //////////////////////////////////////////////
+; Button_A
+; Button_B
+; Button_X
+; Button_Y
+; Button_L
+; Button_R
+; Button_ZL
+; Button_ZR
+; Stick_L_Click
+; Stick_L_Vector2f (double word)
+; Stick_R_Click
+; Stick_R_Vector2f (double word)
+; Button_Select
+; Button_Select_2
+; Button_Start
+; Button_Start_2
+; Button_Touchscreen
+; Button_DPad_Down
+; TimeSinceLastInput (double word)
 
 ; Advanced Info:
 ;
-; The offsets in the table are actually tracking how long a button has been held for. When it
-; equals a register set to 0x00000000, that means it's been held for 0 frames (not pressed).
-; Each button gets 2 bytes for counting how long it's been held, which is why the code only
-; loads half words. The value for any given button will increment by 0x01 each frame. Anyways,
-; you could use this knowledge to set up an event that will only trigger if you hold a set of
-; buttons for 1 second, or something like that.
+; These names are just variables, representing hex values. Those values are offsets (from r3) to data
+; tracking how long a button has been held for. When it equals a register set to 0x00000000, that
+; means it's been held for 0 frames (not pressed). Each button gets 2 bytes for counting how long
+; it's been held, which is why the code only loads half words. The value for any given button will
+; increment by 0x01 each frame. You could use this knowledge to set up an event that will only trigger
+; if you hold a set of buttons for 1 second, or something like that.
 ;
-; There's another set of offsets starting at 0x68, which appear to be in the same order as
-; the ones in the table, 2 bytes apart. These ones appear to increment by 0x222 each frame, which
-; is a little weird. The half-words at 0x38, 0x48, 0x4C, and 0x4E are in the same section as
-; the other offsets listed in the table, but don't seem to correspond to any button.
+; The half-words at 0x38, 0x48, 0x4C, and 0x4E are in the same section as the others listed above, but
+; don't seem to correspond to any button. There's another set of offsets starting at 0x68, which appear
+; to be in the same order (in memory) as the ones above, also half words. These ones appear to increment
+; by 0x222 each frame, which is a little weird.
 ;
-; The 8 bytes from 0x50 to 0x57 are vector2f data for the left stick, and the 8 bytes from 0x58
-; to 0x5F are vector2f data for the right stick. The bytes at 0x10D and 0x10C will have different
-; values depending on the rotation of the left and right stick respectively, but only if they're
-; past a certain threshold from the center.
-;
-; Also, there's a section you can find at 0x108 which increments by 0x01 every frame, and resets to
-; 0 when ANY input is detected.
+; The bytes at 0x10D and 0x10C will have different values depending on the rotation of the left and
+; right stick respectively, but only if they're past a certain threshold from the center.
 ;
 
 
@@ -109,3 +89,23 @@ addi r5, r5, SafeStringEntryPoint@l
 li r6, 0                            ; bool isPauseOtherActors (overridden by eventinfo)
 li r7, 0                            ; bool skipIsStartableAirCheck (overridden by eventinfo)
 blr
+
+Button_A = 0x28
+Button_B = 0x2A
+Button_X = 0x2E
+Button_Y = 0x30
+Button_L = 0x42
+Button_R = 0x44
+Button_ZL = 0x2C
+Button_ZR = 0x32
+Stick_L_Click = 0x36
+Stick_L_Vector2f = 0x50
+Stick_R_Click = 0x34
+Stick_R_Vector2f = 0x58
+Button_Select = 0x3A
+Button_Select_2 = 0x40
+Button_Start = 0x3C
+Button_Start_2 = 0x3E
+Button_Touchscreen = 0x46
+Button_DPad_Down = 0x4A
+TimeSinceLastInput = 0x108
