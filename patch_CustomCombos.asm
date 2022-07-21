a1cc4b7b3608a7a443f7c17201d8330193661e96
Torphedo <ahiatt.cosgrove@gmail.com>
2022-07-21 04:52:22 -0400

    Store ComboLR sooner

--- patch_CustomCombos.asm
+++ patch_CustomCombos.asm
@@ -22,6 +22,10 @@ bla Combo2
 b ExitCodecave
 
 Combo1:
+mflr r15
+lis r14, StoreComboLR@ha
+stw r15, StoreComboLR@l(r14)
+
 lis r15, EventName@ha
 addi r15, r15,  EventName@l
 lis r16, EntryPoint@ha
@@ -33,9 +37,6 @@ stw r14, 0x0(r15)
 lis r14, FaroreEntryPoint@ha
 addi r14, r14,  FaroreEntryPoint@l
 stw r14, 0x0(r16)
-mflr r15
-lis r14, StoreComboLR@ha
-stw r15, StoreComboLR@l(r14)
 ; Read controller data
 bla Check_R
 bla Check_ZR
@@ -44,6 +45,10 @@ bla Check_Y
 b PlayEvent
 
 Combo2:
+mflr r15
+lis r14, StoreComboLR@ha
+stw r15, StoreComboLR@l(r14)    ; Stash LR data
+
 lis r15, EventName@ha
 addi r15, r15,  EventName@l
 lis r16, EntryPoint@ha
@@ -55,9 +60,6 @@ stw r14, 0x0(r15)
 lis r14, DogEntryPoint@ha       ; String defined at
 addi r14, r14,  DogEntryPoint@l ; the top of the file
 stw r14, 0x0(r16)
-mflr r15
-lis r14, StoreComboLR@ha
-stw r15, StoreComboLR@l(r14)    ; Stash LR data
 ; Read controller data
 bla Check_L
 bla Check_ZL
